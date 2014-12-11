bbda387c0d9ce9e2b9aeac7a6add4b3d4ad885e7
AmnaAmer <aiaa1g13@ecs.soton.ac.uk>
2014-12-11 14:40:49 +0000

    Updated code
    updated comments and indentations

--- about.php
+++ about.php
@@ -1,47 +1,49 @@
-		<?php 
+	<?php 
 		require_once('includes/predispatch.php');
 		require_once('includes/header.php');
 		require('includes/db.php');
-
-		//gets the film class
-		require_once('classes/film.class.php');
-		
-		// query to select from the film table a random film 
-		$films = $db->query("SELECT * FROM `film` ORDER BY RAND() LIMIT 1");
-		?>
-		
-		<?php
-			// Queries the film table for latest films
-			$filmposter = $db->query("SELECT * FROM `film` ORDER BY theatricalRelease DESC LIMIT 5"); 
-		?>
-		
-		 <?php
-		    if($filmposter->num_rows > 0){
-		
-		        while($poster = $filmposter -> fetch_assoc()){
-		          $id_film[] = $poster ['id'];
-		        }
-		    }
-		
-		  ?>
-
-		  <!-- carousel included within a partial-->
-		   <?php include('partials/carousel.php');?>
-
-			<!--Welcome Message--> 
-			<div id="course-list" class="row" >
-				<div class="list-group row text-center" style="color:#F0F8FF;">
-				<p>Bring a Smile to somebody's face with Red Carpet Films</p>
-				<p>Red Carpet Films provides all the facts you need for a range of films. Share your thoughts and find out what others think!</p>
-				<p>Get lost in a world where anything is possible... from guardians who protect the galaxy to a winter wonderland in a wardrobe.</p>
-				<p>IT'S FREE AND SIMPLE!</p>
-
-			<?php
-					// Film of the click; random film presented each time it is refreshed
-		            while($film = $films->fetch_object('Film')): 
-		                include('partials/filmotd.php');?>
-						 <?php endwhile; ?>	 
-				</div>
-			</div>
-
-		<?php require_once('includes/footer.php'); ?>
+	
+	//gets the film class
+	require_once('classes/film.class.php');
+	
+	// query to select from the film table a random film 
+	$films = $db->query("SELECT * FROM `film` ORDER BY RAND() LIMIT 1");
+	?>
+	
+	<?php
+	// Queries the film table for latest films
+		$filmposter = $db->query("SELECT * FROM `film` ORDER BY theatricalRelease DESC LIMIT 5"); 
+	?>
+	
+	<?php
+		if($filmposter->num_rows > 0){
+	
+		while($poster = $filmposter -> fetch_assoc()){
+		$id_film[] = $poster ['id'];
+		}
+	}
+	
+	?>
+	
+	<!-- carousel included within a partial-->
+	<?php include('partials/carousel.php');?>
+	
+	
+	<!--Welcome Message--> 
+	<div id="course-list" class="row" >
+		<div class="list-group row text-center" style="color:#F0F8FF;">
+			<p>Bring a Smile to somebody's face with Red Carpet Films</p>
+			<p>Red Carpet Films provides all the facts you need for a range of films. Share your thoughts and find out what others think!</p>
+			<p>Get lost in a world where anything is possible... from guardians who protect the galaxy to a winter wonderland in a wardrobe.</p>
+			<p>IT'S FREE AND SIMPLE!</p>
+	
+	<?php
+	
+		// Film of the click; random film presented each time it is refreshed
+		while($film = $films->fetch_object('Film')): 
+			include('partials/filmotd.php');?>
+				<?php endwhile; ?>	 
+		</div>
+	</div>
+	
+	<?php require_once('includes/footer.php'); ?>
