ce3c6264c94845fe75045b9b44686d1522ec4660
AmnaAmer <aiaa1g13@ecs.soton.ac.uk>
2014-12-11 14:43:26 +0000

    updated code
    updated comments and indentation in;
    
    process review, show-film and show-genre

--- show-film.php
+++ show-film.php
@@ -1,146 +1,144 @@
-	<?php
-	require_once('includes/predispatch.php');
-	require_once('includes/header.php');
-	require('includes/db.php');
-	// get the film and review classes
-	require_once('classes/film.class.php');
-	require_once('classes/review.class.php');
-	?>
-	
-	<!-- main content -->
-	<div id="content" class="row">
-
 		<?php
-		try{
-			// check that the ID exists and it is the correct type
-			// Throwing an expception ensures messages appears if there is no connection
-			if(!isset($_GET['id']) OR empty($_GET['id']) OR !is_numeric($_GET['id'])) throw new Exception('No connection found');
-			
-			// get the genre we want to use
-			// ensuring we escape the GET string
-			$film = $db->query('SELECT * FROM film WHERE id = '.$db->real_escape_string($_GET['id']))->fetch_object('Film');
-			
-			// get a film using a specific ID
-			$films = $db->query('SELECT * FROM film WHERE id= '.$db->real_escape_string($_GET['id']));
-			?>
-				<!--Films List -->			
-				<div id="film-list" class="row">
-				
-				<?php while($film = $films->fetch_object("Film")): ?>
-					<?php $rtID = $film->rt_id; ?>
-					<div class="film-item">
-						<div class="row">
-							<div class="col-md-8 font-weight:bold" ><h3><?=$film->name?></h3></div>
-							<div class="col-md-4 pull-right"><a href="filminfo.php?id=<?=$film->id?>" class="btn btn-primary pull-right" id="button">Find out more?</a></div>
-						</div>
-							
-						<div class="row">
-							  <?php echo "<img src='http://comp2203.ecs.soton.ac.uk/coursework/1415/assets/posters/".$film->id."_medium.jpg'>" ?>
-							<div class="col-md-8"><strong><?=$film->description?></strong></div>
-							
-						</div>
-					</div>
+		require_once('includes/predispatch.php');
+		require_once('includes/header.php');
+		require('includes/db.php');
+		// get the film and review classes
+		require_once('classes/film.class.php');
+		require_once('classes/review.class.php');
 
-				<?php endwhile; ?>
-			</div>
-
-			<div class= "row tomatoes"> 
-	<div class="col-md-12"> 
-	<?php 
-
-	/*Uses ones rotten tomatoes key to obtain data from the site regarding the films*/
-	$json = file_get_contents('http://api.rottentomatoes.com/api/public/v1.0/movies/' . $rtID .'.json?apikey=q2rhkz62np7gkn6abcf83bkg'); 
-	$films = json_decode($json);
-
-	// Extracting critic's and audience's score from the data
-	$criticsScore = $films->ratings->critics_score;
-	$audienceScore = $films->ratings->audience_score;
-
-		// prints only the critics and audience's score
-		
-		echo "<br></br>";
-		echo "Audience's Score:";
-		echo $audienceScore;
-		 /*echo '<pre>'; 
-	 	print_r($films); 
-		echo '</pre>';*/ 
-
-	 ?>
-	 <!-- Critic's and Audience's scores progress bars -->
-	<?php include('partials/progress-bars.php');?>
-						
-	</div>
-
-
-						
-	</div>
-</div>
-
-<div style="font-size: 14px; font-weight:bold;">
-			<!-- Obtains up to 10 reviews from the database-->
-			<?php $reviews = $db->query('SELECT * FROM review WHERE film_id= '.$db->real_escape_string($_GET['id']).' LIMIT 10'); ?>
-				<h3>Current Thoughts</h3>
-				<table class="table table-striped">
-					<thead>
-						<tr>
-							<!-- Heading included within the review table -->
-							<th>Reviewer</th>
-							<th>Thoughts</th>
-							<th>Liked or Disliked</th>
-						</tr>
-					</thead>
-					<body>
-						<!-- Fetches the Reviews from database -->
-						<?php while($review = $reviews->fetch_object("Review")): ?>
-							<tr>
-								<!--Specifies which data I want from the data-->
-								<td><?=$review->reviewer?></td>
-								<td><?=$review->comment?></td>
-								<td>
-									<?php
-									// provide a liked or disliked symbol depending on the results
-									if($review->liked == 1){
-									echo ("<span class=' glyphicon glyphicon-ok'></span>");
-									} else{
-										echo ("<span class='glyphicon glyphicon-remove'></span>");
-									}?>
-
-								</td>
-							</tr>
-						<?php endwhile; ?>
-					</tbody>
-				</table>
-			</div>
-
-<div class="row">
-			<!--Adding a review form-->
-			<h3>Share your opinion!</h3>
-
-			<!--User can enter thier personal details and thoughts regarding a specific film -->
-			<form action="process-review.php" method="post">
-			<input type="hidden" name="film_id" value="<?=$_GET['id']?>">
-			Name: <input type="text" name="reviewer">
-			Thoughts: <input type="text" name="comment">
-			Like?:<input type="checkbox" name="like" value= "1">
-			Dislike?:<input type="checkbox" name="like" value= "0">
-			<input type="submit" value="Submit">
-			</form>
-
-		</div>
-
-	</div>
-
-	</div>
-		<?php
-		}
-		catch(Exception $e){
-			echo '<div class="alert alert-danger">';
-			echo 	'<strong>Error!</strong>';
-			echo 	'<p>'.$e->getMessage().'</p>';
-			echo '</div>';
-		}
-		
+		//prevents the errors from appearing on the films, where no information can be provided from rotten tomatoes API
+		error_reporting(0);
 		?>
 		
+		<!-- main content -->
+		<div id="content" class="row">
+	
+			<?php
+			try{
+				// check that the ID exists and it is the correct type
+				// Throwing an expception ensures messages appears if there is no connection
+				if(!isset($_GET['id']) OR empty($_GET['id']) OR !is_numeric($_GET['id'])) throw new Exception('No connection found');
+				
+				// get the genre we want to use
+				// ensuring we escape the GET string
+				$film = $db->query('SELECT * FROM film WHERE id = '.$db->real_escape_string($_GET['id']))->fetch_object('Film');
+				
+				// get a film using a specific ID
+				$films = $db->query('SELECT * FROM film WHERE id= '.$db->real_escape_string($_GET['id']));
+				?>
+					<!--Films List -->			
+					<div id="film-list" class="row">
+					
+					<?php while($film = $films->fetch_object("Film")): ?>
+						<?php $rtID = $film->rt_id; ?>
+						<div class="film-item">
+							<div class="row">
+								<div class="col-md-8 font-weight:bold" ><h3><?=$film->name?></h3></div>
+								<div class="col-md-4 pull-right"><a href="filminfo.php?id=<?=$film->id?>" class="btn btn-primary pull-right" id="button">Find out more?</a></div>
+							</div>
+								
+							<div class="row">
+								  <?php echo "<img src='http://comp2203.ecs.soton.ac.uk/coursework/1415/assets/posters/".$film->id."_medium.jpg'>" ?>
+								<div class="col-md-8"><strong><?=$film->description?></strong></div>
+								
+							</div>
+						</div>
+	
+					<?php endwhile; ?>
+				</div>
+	
+	<div class= "row tomatoes"> 
+		<div class="col-md-12"> 
+		<?php 
+	
+		/*Uses ones rotten tomatoes key to obtain data from the site regarding the films*/
+		$json = file_get_contents('http://api.rottentomatoes.com/api/public/v1.0/movies/' . $rtID .'.json?apikey=q2rhkz62np7gkn6abcf83bkg'); 
+		$films = json_decode($json);
+	
+		// Extracting critic's and audience's score from the data
+		$criticsScore = $films->ratings->critics_score;
+		$audienceScore = $films->ratings->audience_score;
+	
+			// prints only the critics and audience's score
+			
+			echo "<br></br>";
+			echo "Audience's Score:";
+			echo $audienceScore;
+
+			// Uncomment the code to allow the user to gain access to all the data provided by Rottten Tomatoes 
+			 /*echo '<pre>'; 
+		 	print_r($films); 
+			echo '</pre>';*/ 
+	
+		 ?>
+		 <!-- Critic's and Audience's scores progress bars -->
+		<?php include('partials/progress-bars.php');?>
+							
+		</div>
 	</div>
-	<?php require_once('includes/footer.php'); ?>
+	<div style="font-size: 14px; font-weight:bold;">
+				<!-- Obtains up to 10 reviews from the database-->
+				<?php $reviews = $db->query('SELECT * FROM review WHERE film_id= '.$db->real_escape_string($_GET['id']).' LIMIT 10'); ?>
+					<h3>Current Thoughts</h3>
+					<table class="table table-striped">
+						<thead>
+							<tr>
+								<!-- Heading included within the review table -->
+								<th>Reviewer</th>
+								<th>Thoughts</th>
+								<th>Liked or Disliked</th>
+							</tr>
+						</thead>
+						<body>
+							<!-- Fetches the Reviews from database -->
+							<?php while($review = $reviews->fetch_object("Review")): ?>
+								<tr>
+									<!--Specifies which data I want from the data-->
+									<td><?=$review->reviewer?></td>
+									<td><?=$review->comment?></td>
+									<td>
+										<?php
+										// provide a liked or disliked symbol depending on the results
+										if($review->liked == 1){
+										echo ("<span class=' glyphicon glyphicon-ok'></span>");
+										} else{
+											echo ("<span class='glyphicon glyphicon-remove'></span>");
+										}?>
+	
+									</td>
+								</tr>
+							<?php endwhile; ?>
+						</tbody>
+					</table>
+				</div>
+	
+	<div class="row">
+				<!--Adding a review form-->
+				<h3>Share your opinion!</h3>
+	
+				<!--User can enter thier personal details and thoughts regarding a specific film -->
+				<form action="process-review.php" method="post">
+				<input type="hidden" name="film_id" value="<?=$_GET['id']?>">
+				Name: <input type="text" name="reviewer">
+				Thoughts: <input type="text" name="comment">
+				Like?:<input type="checkbox" name="like" value= "1">
+				Dislike?:<input type="checkbox" name="like" value= "0">
+				<input type="submit" value="Submit">
+				</form>
+	
+			</div>
+	
+		</div>
+			<?php
+			}
+			catch(Exception $e){
+				echo '<div class="alert alert-danger">';
+				echo 	'<strong>Error!</strong>';
+				echo 	'<p>'.$e->getMessage().'</p>';
+				echo '</div>';
+			}
+			
+			?>
+			
+		</div>
+		<?php require_once('includes/footer.php'); ?>
--- show-genre.php
+++ show-genre.php
@@ -1,60 +1,60 @@
-<?php
-require_once('includes/predispatch.php');
-require_once('includes/header.php');
-
-// get the database connection
-require('includes/db.php');
-// get the genre and film classes
-require_once('classes/genre.class.php');
-require_once('classes/film.class.php');
-?>
-<!-- main content of webpage-->
-<div id="content" class="row">
 	<?php
-	try{
-		// check that the ID exists and it is the correct type
-		// Throwing an expception ensures messages appears if there is no connection 
-		if(!isset($_GET['id']) OR empty($_GET['id']) OR !is_numeric($_GET['id'])) throw new Exception('No connection');
-		
-		// get the genre we want to use
-		// ensuring we escape the GET string
-		$genre = $db->query('SELECT * FROM genre WHERE id = '.$db->real_escape_string($_GET['id']))->fetch_object('genre');;
-		
-		// get all of the films by a specific genre id 
-		$films = $db->query('SELECT * FROM film WHERE genre_id= '.$db->real_escape_string($_GET['id']));
-		
-		?>
-
-		<!-- Film's list -->
-		<div id="film-list" class="row">
-			<?php while($film = $films->fetch_object("Film")): ?>
-				<div class="film-item">
-					<div class="row">
-						<div class="col-md-6"><h3><?=$film->name?></h3></div>
-						<!-- Button links to info.php, where more information regarding the film is stored -->
-						<div class="col-md-4 pull-right"><a href="info.php?id=<?=$film->id?>" class="btn btn-primary pull-right">Find out more!</a></div>
-					</div>
-						
-					<div class="row">
-						<!-- Displays the poster of the films-->
-						<?php echo "<img src='http://comp2203.ecs.soton.ac.uk/coursework/1415/assets/posters/".$film->id."_small.jpg'>" ?>
-						<!-- Take decription and film classification from the database and displays them on the webpage -->
-						<div class="col-md-6"><strong><?=$film->description?></strong></div>
-						<div class="col-md-4 pull-right"><p class="film-classification pull-right"><strong><?=$film->classification?></strong><p></div>
-					</div>
-				</div>
-			<?php endwhile; ?>
-		</div>
-	<?php
-	}
-
-	catch(Exception $e){
-		echo '<div class="alert alert-danger">';
-		echo 	'<strong>Error!</strong>';
-		echo 	'<p>'.$e->getMessage().'</p>';
-		echo '</div>';
-	}
+	require_once('includes/predispatch.php');
+	require_once('includes/header.php');
+	
+	// get the database connection
+	require('includes/db.php');
+	// get the genre and film classes
+	require_once('classes/genre.class.php');
+	require_once('classes/film.class.php');
 	?>
-
-</div>
-<?php require_once('includes/footer.php'); ?>
+	<!-- main content of webpage-->
+	<div id="content" class="row">
+		<?php
+		try{
+			// check that the ID exists and it is the correct type
+			// Throwing an expception ensures messages appears if there is no connection 
+			if(!isset($_GET['id']) OR empty($_GET['id']) OR !is_numeric($_GET['id'])) throw new Exception('No connection');
+			
+			// get the genre we want to use
+			// ensuring we escape the GET string
+			$genre = $db->query('SELECT * FROM genre WHERE id = '.$db->real_escape_string($_GET['id']))->fetch_object('genre');;
+			
+			// get all of the films by a specific genre id 
+			$films = $db->query('SELECT * FROM film WHERE genre_id= '.$db->real_escape_string($_GET['id']));
+			
+			?>
+	
+			<!-- Film's list -->
+			<div id="film-list" class="row">
+				<?php while($film = $films->fetch_object("Film")): ?>
+					<div class="film-item">
+						<div class="row">
+							<div class="col-md-6"><h3><?=$film->name?></h3></div>
+							<!-- Button links to info.php, where more information regarding the film is stored -->
+							<div class="col-md-4 pull-right"><a href="info.php?id=<?=$film->id?>" class="btn btn-primary pull-right">Find out more!</a></div>
+						</div>
+							
+						<div class="row">
+							<!-- Displays the poster of the films-->
+							<?php echo "<img src='http://comp2203.ecs.soton.ac.uk/coursework/1415/assets/posters/".$film->id."_small.jpg'>" ?>
+							<!-- Take decription and film classification from the database and displays them on the webpage -->
+							<div class="col-md-6"><strong><?=$film->description?></strong></div>
+							<div class="col-md-4 pull-right"><p class="film-classification pull-right"><strong><?=$film->classification?></strong><p></div>
+						</div>
+					</div>
+				<?php endwhile; ?>
+			</div>
+		<?php
+		}
+	
+		catch(Exception $e){
+			echo '<div class="alert alert-danger">';
+			echo 	'<strong>Error!</strong>';
+			echo 	'<p>'.$e->getMessage().'</p>';
+			echo '</div>';
+		}
+		?>
+	
+	</div>
+	<?php require_once('includes/footer.php'); ?>
